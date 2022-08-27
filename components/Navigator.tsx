e7c2912a14d5b2d288dea80a3f22d6f17238c897
mayrang <pkss0626@naver.com>
2022-08-27 19:34:22 +0900

    Nav design, select design

---
+++ components/Navigator.tsx
@@ -0,0 +1,16 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faArrowLeft} from "@fortawesome/free-solid-svg-icons"
+import styles from "../styles/Navigator.module.css";
+
+const Navigator = () => {
+    return (
+        <header>
+            <nav className={styles.nav}>
+                <div className={styles.leftIcon}><FontAwesomeIcon icon={faArrowLeft} size="lg"/></div>
+                <div className={styles.logo}>Home</div>
+            </nav>
+        </header>
+    );
+};
+
+export default Navigator;
