0bb4cdee104a23f200f5c27af557d8ba64fa9446
mayrang <pkss0626@naver.com>
2022-08-29 22:48:40 +0900

    api 요청 테스트

--- pages/station/[id].tsx
+++ pages/station/[id].tsx
@@ -5,6 +5,7 @@ import {faArrowLeft} from "@fortawesome/free-solid-svg-icons"
 import { useCallback } from "react";
 import stationList from "../../utils/stationList";
 import { GetStaticPaths, GetStaticProps } from "next";
+import axios from "axios";
 
 interface StationProps {
     stationName: string;
@@ -37,12 +38,15 @@ export const getStaticPaths:GetStaticPaths = () => {
     };
 };
 
-export const getStaticProps:GetStaticProps = ({params}) => {
+export const getStaticProps:GetStaticProps = async ({params}) => {
     const stationName = stationList.find((it) => it.id === params?.id)?.name;
+    const result = await axios.get(`http://www.djtc.kr/OpenAPI/service/TimeTableSVC/getTimeTable?serviceKey=${process.env.API_SERVICE_KEY}&stNum=${params?.id}&dayType=0&drctType=0`);
+    console.log(result.data.response.body.items.item);
     return {
         props: {
             stationName
         }
-    }
-}
+    };
+};
+
 
